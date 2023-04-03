bd027d527c00d9d12082e8c099617a58bd21b696
magoyoo <magoyoooooo@gmail.com>
2023-04-03 12:10:04 +0900

    4-1 상하좌우 연습문제

---
+++ practiceCodingTest/bookExample/Chapter4/lrud.swift
@@ -0,0 +1,41 @@
+//
+//  lrud.swift
+//  PracticeCodingTest
+//
+//  Created by 김유준 on 2023/04/03.
+//
+
+import Foundation
+
+func searchLrud(_ firstLine: String, _ secondLine: String){
+    var coordinate: [Int] = [1, 1]
+    let n = Int(firstLine)!
+    let moveArray = secondLine
+                        .split(separator: " ")
+                        .compactMap{ String($0) }
+    
+    
+    for move in moveArray{
+        switch move {
+        case "R":
+            if coordinate[0] < n {
+                coordinate[0] += 1
+            }
+        case "L":
+            if coordinate[0] > 1 {
+                coordinate[0] -= 1
+            }
+        case "U":
+            if coordinate[1] > 1 {
+                coordinate[1] -= 1
+            }
+        case "D":
+            if coordinate[1] < n {
+                coordinate[1] += 1
+            }
+        default: return
+        }
+    }
+    
+    print("\(coordinate[1]) \(coordinate[0])")
+}
--- practiceCodingTest/main.swift
+++ practiceCodingTest/main.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-// MARK: - Gird
+// MARK: -  Chapter4 Greedy
 
 // 1. 거스름돈
 //let firstLine = readLine()!
@@ -46,10 +46,16 @@ import Foundation
 
 // 4. 1이 될 때까지
 
-let firstLine = readLine()!
- 
+//let firstLine = readLine()!
+//
+//
+//excuteFunc(
+//    firstLine,
+//    untilOne
+//)
 
-excuteFunc(
-    firstLine,
-    untilOne
-)
+// MARK: - Chapter4 구현
+let firstLine = readLine()!
+let secondLine = readLine()!
+
+searchLrud(firstLine, secondLine)
