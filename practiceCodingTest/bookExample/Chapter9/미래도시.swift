1ae56470c64ecf420741cc0fade97d8e58f49580
magoyoo <magoyoooooo@gmail.com>
2023-04-26 20:36:41 +0900

    이것이 코딩테스트다 문제 풀이 - 미래 도시

--- practiceCodingTest/bookExample/Chapter9/미래도시.swift
+++ practiceCodingTest/bookExample/Chapter9/미래도시.swift
@@ -1,1 +1,49 @@
 import Foundation
+
+func futureCity(){
+    let firstInput = readLine()!.split(separator: " ").map { Int($0)! }
+    let n = firstInput[0]
+    let m = firstInput[1]
+    var graph: [[Int]] = Array(repeating: Array(repeating: Int.max, count: n), count: n)
+    
+    for _ in 0..<m {
+        let input = readLine()!.split(separator: " ").map { Int($0)! }
+        let startNode = input[0] - 1
+        let endNode = input[1] - 1
+        graph[startNode][endNode] = 1
+        graph[endNode][startNode] = 1
+    }
+    
+    let xk = readLine()!.split(separator: " ").map { Int($0)! }
+    let xIndex = xk[0] - 1
+    let kIndex = xk[1] - 1
+    
+    for k in 0..<n { // 거쳐가는 곳
+        for i in 0..<n {
+            for j in 0..<n {
+                if i == j || i == k || j == k {
+                    continue
+                }
+                
+                if graph[i][k] == Int.max || graph[k][j] == Int.max {
+                    continue
+                }
+                
+                if graph[i][j] > graph[i][k] + graph[k][j] {
+                    graph[i][j] = graph[i][k] + graph[k][j]
+                }
+            }
+        }
+    }
+    
+    var result: Int {
+        if graph[xIndex][kIndex] == Int.max || graph[0][kIndex] == Int.max {
+            return -1
+        } else {
+            return graph[xIndex][kIndex] + graph[0][kIndex]
+        }
+    }
+    
+    print(result)
+}
+
--- practiceCodingTest/main.swift
+++ practiceCodingTest/main.swift
@@ -1,13 +1,2 @@
 import Foundation
 
-
-fileprivate let graph: [[Int]] = [
-    [0, 4, Int.max, 6],
-    [3, 0, 7, Int.max],
-    [5, Int.max, 0, 4],
-    [Int.max, Int.max, 2, 0]
-]
-
-fileprivate let numNodes: Int = 4
-
-print(floydWarshall(graph, numNodes))
